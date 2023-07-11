83c287a25e0eaed349aeb7a33d7df304091ba29a
cybergrazi <graziellecvergilio@gmail.com>
2023-07-11 20:39:27 -0300

    Alteração do valor na variável populacaoSP.

--- unidade-1/aula-03-variaveis/exercicios/exercicio-02.js
+++ unidade-1/aula-03-variaveis/exercicios/exercicio-02.js
@@ -1,9 +1,9 @@
 //Declarar uma variável mutável , guardar nela a população da cidade em que eu moro e imprimir.
 //Depois, modificar o valor dessa variável e imprimir novamente.
 
-let populacaoSP = 11;
+let populacaoSP = 11_451_245;
 
-console.log(`A população da cidade de São Paulo em 2023 é de aproximadamente ${populacaoSP} milhões de pessoas.`);
+console.log(`A população da cidade de São Paulo em 2023 é de ${populacaoSP} de pessoas.`);
 
 populacaoSP = 1;
 
