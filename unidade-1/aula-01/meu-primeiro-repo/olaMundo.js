e34f07f46171593a6e09172f62e265e303837456
cybergrazi <graziellecvergilio@gmail.com>
2023-07-11 19:37:52 -0300

    README indesejado removido, 2 novos exercícios para aula 2

--- unidade-1/aula-01/meu-primeiro-repo/olaMundo.js
+++ unidade-1/aula-01/meu-primeiro-repo/olaMundo.js
@@ -1,2 +1,5 @@
+//Primeira aula do curso de back-end da Cubos Academy, atividade de criar um novo repositório no GitHub 
+//e interagir com as ferramentas Git e VsCode.
+
 console.log("Olá Mundo ♥");
 console.log("你好 ♥")
