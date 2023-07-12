899214adef790fc94376a6ba597a93f7b3af80d0
cybergrazi <graziellecvergilio@gmail.com>
2023-07-12 19:24:26 -0300

    Tipos de variáveis adicionado

---
+++ unidade-1/aula-03-variaveis/variaveis-tipos.js
@@ -0,0 +1,13 @@
+// Apresentação de tipos de variáveis básicas
+let nome = "Grazi"; // String
+let idade = 28; // Number
+let altura = 1.69; // Number
+
+let nota; // Undeifned
+console.log(nota); // Undefined
+
+nota = 10;
+console.log(nota);
+
+nota = null; // Null
+console.log(nota);
