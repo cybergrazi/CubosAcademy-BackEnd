900a87e7446686e8a2b4f1c507cba6cd6ff0172b
cybergrazi <graziellecvergilio@gmail.com>
2023-07-12 20:17:19 -0300

    Operadores aritméticos adicionado

---
+++ unidade-1/aula-03-variaveis/variaveis-aritmeticos.js
@@ -0,0 +1,16 @@
+// Introdução aos operadores aritméticos do JavaScript
+
+// Declarando 2 variáveis na mesma linha:
+let x = 10, y = 2;
+
+let resultado = x + y;
+console.log(`A soma é ${resultado}`);
+
+resultado = x - y;
+console.log(`A subtração é ${resultado}`);
+
+resultado = x * y;
+console.log(`A multiplicação é ${resultado}`);
+
+resultado = x / y;
+console.log(`A divisão é ${resultado}`);
