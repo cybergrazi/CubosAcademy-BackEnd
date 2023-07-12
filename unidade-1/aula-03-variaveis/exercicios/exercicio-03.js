c91c7599f20e79e090f41e709c8bdea097b1207a
cybergrazi <graziellecvergilio@gmail.com>
2023-07-12 20:49:38 -0300

    Criação e resolução do exercício 3

---
+++ unidade-1/aula-03-variaveis/exercicios/exercicio-03.js
@@ -0,0 +1,15 @@
+// Com base na aula variaveis-aritmeticos e variaveis-concatenacao, imprimir juntamente com texto.
+
+let x = 10, y = 2;
+
+let resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}`);
+
+resultado = x - y;
+console.log(`A subtração ${x} e ${y} é ${resultado}`);
+
+resultado = x * y;
+console.log(`A multiplicação de ${x} e ${y} é ${resultado}`);
+
+resultado = x / y;
+console.log(`A divisão de ${x} e ${y} 7 é ${resultado}`);
