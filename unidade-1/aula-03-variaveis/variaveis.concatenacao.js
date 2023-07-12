ae67cd625a863be929851db249ca431ba24b000d
cybergrazi <graziellecvergilio@gmail.com>
2023-07-12 20:25:07 -0300

    Criação de concatenação

---
+++ unidade-1/aula-03-variaveis/variaveis.concatenacao.js
@@ -0,0 +1,7 @@
+// Introdução à concatenação de variáveis: strings e numbers
+
+let nome = "Grazielle";
+let sobrenome = "Vidal";
+
+let nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
