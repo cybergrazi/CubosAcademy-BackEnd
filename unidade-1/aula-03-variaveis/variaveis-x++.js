acd15e4941fbfea197171234e7a1c069b0f53ef2
cybergrazi <graziellecvergilio@gmail.com>
2023-07-12 21:38:43 -0300

    Introdução aos operadores x++ e ++x

---
+++ unidade-1/aula-03-variaveis/variaveis-x++.js
@@ -0,0 +1,17 @@
+// Introdução aos operadores "++" e "--".
+
+let x = 10;
+
+//x = x + 1;
+//x += 1;
+x++; //11
+console.log(x);
+
+//x = x - 1;
+//x -= 1;
+x--; //10
+console.log(x);
+
+let y = ++x;
+console.log("X=", x);
+console.log("Y=", y);
