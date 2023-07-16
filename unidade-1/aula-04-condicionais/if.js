929ab80e0dfdd79fbbf622c49092ea7df656a699
cybergrazi <graziellecvergilio@gmail.com>
2023-07-16 17:47:57 -0300

    Introdução ao if e else

---
+++ unidade-1/aula-04-condicionais/if.js
@@ -0,0 +1,11 @@
+// Introdução ao if e else.
+let idade = 8;
+
+if (idade >= 18)
+    //é executado caso a condição seja verdadeira
+    console.log("Maior de idade");
+else { //utiliza-se chaves pois é executado mais de 1 linha
+    //se a anterior for falsa, executa este bloco de código
+    console.log("Menor de idade");
+    console.log("Então não pode beber")
+}
