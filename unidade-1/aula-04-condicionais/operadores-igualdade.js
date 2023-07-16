41f4109a31d2591983cfe63bc41fb4ddc6338805
cybergrazi <graziellecvergilio@gmail.com>
2023-07-16 16:23:15 -0300

    introdução aos operadores de igualdade

---
+++ unidade-1/aula-04-condicionais/operadores-igualdade.js
@@ -0,0 +1,7 @@
+// Introdução aos operadores de igualldade
+
+const ehIgual = 18 == 18; // == igual que
+console.log(ehIgual);
+
+const ehDiferente = 18 != 18; // != diferente que
+console.log(ehDiferente);
