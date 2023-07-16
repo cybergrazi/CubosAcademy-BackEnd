9c18013ed78bb15e39d2db3e9f2a48093ac0392c
cybergrazi <graziellecvergilio@gmail.com>
2023-07-16 17:55:47 -0300

    Introdução ao else if

---
+++ unidade-1/aula-04-condicionais/if-else-if.js
@@ -0,0 +1,14 @@
+// Introdução ao else if.
+
+const idade = 10;
+
+if (idade < 18) {
+    console.log("Menor de idade");
+    console.log("Não pode beber");
+} else if (idade >= 65) {
+    console.log("Idoso");
+    console.log("Dar preferência à saúde");
+} else {
+    console.log("Adulto");
+    console.log("Pode beber");
+}
