9a09f38dfda572f4e4e31f3fd31315aad865fd27
cybergrazi <graziellecvergilio@gmail.com>
2023-07-16 16:17:01 -0300

    aula realizada

---
+++ unidade-1/aula-04-condicionais/operadores-comparacao.js
@@ -0,0 +1,20 @@
+// Introdução aos comparadores de comparação (maior e menor que...).
+
+const ehMaior = 25 > 16; //Maior que
+console.log(ehMaior);
+
+const ehMenor = 15 < 16; //Menor que
+console.log(ehMenor);
+
+const maiorIgual = 16 >= 16; //Maior ou igual que
+console.log(maiorIgual);
+
+const menorIgual = 15 <= 16; //Menor ou igual que
+console.log(menorIgual);
+
+//Testando com variáveis
+
+const x = 8;
+const y = 16;
+const ehMaiorQue = x >= y;
+console.log(ehMaiorQue);
