5f4c3bb762e3a789a80d3845543efe858584ab56
cybergrazi <graziellecvergilio@gmail.com>
2023-07-19 20:59:37 -0300

    Introdução ao conceito de Arrays

---
+++ unidade-1/aula-05-arrays-e-loops/introducao-arrays.js
@@ -0,0 +1,17 @@
+// Introdução ao conceito de Arrays.
+
+// const nome1 = "Maria";
+// const nome2 = "José";
+// const nome3 = "Fernanda";
+
+const nomeEstudantes = ["Maria", "José", "Fernanda", "Catarina", "Elizabeth"];
+const notaEstudantes = [9, 10, 8, 6, 5];
+
+console.log(nomeEstudantes);
+console.log(nomeEstudantes[0]);
+console.log(nomeEstudantes[1]);
+console.log(nomeEstudantes[2]);
+console.log(nomeEstudantes[3]);
+console.log(nomeEstudantes[4]);
+console.log(nomeEstudantes[5]); //undefined
+console.log(notaEstudantes);
