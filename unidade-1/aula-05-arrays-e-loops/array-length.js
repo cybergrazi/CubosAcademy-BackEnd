cf222d4122d5d1ed7009e6a1a8ceaaa729c7cbb1
cybergrazi <graziellecvergilio@gmail.com>
2023-07-19 21:15:46 -0300

    Introdução ao conceito de length do array

---
+++ unidade-1/aula-05-arrays-e-loops/array-length.js
@@ -0,0 +1,11 @@
+// Conceito de length do array.
+
+const nomeEstudantes = ["Maria", "José", "Fernanda", "Catarina", "Elizabeth"];
+
+// verificando o tamanho do array
+
+console.log(nomeEstudantes.length); // lenght = propriedade deste array referente ao tamanho
+
+// incrementando mais um valor para o array. Essa é a forma mais recomendada pois sempre estará no final.
+nomeEstudantes[nomeEstudantes.length] = "Patricia";
+console.log(nomeEstudantes); 
