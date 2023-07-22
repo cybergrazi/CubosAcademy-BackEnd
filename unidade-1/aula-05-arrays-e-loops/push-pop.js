9cbabed2cafdd1aa847fee5d1f6930b61ea07718
cybergrazi <graziellecvergilio@gmail.com>
2023-07-21 23:16:08 -0300

    Introdução a Push e Pop

---
+++ unidade-1/aula-05-arrays-e-loops/push-pop.js
@@ -0,0 +1,13 @@
+// Introdução a Push e Pop.
+
+const nomeEstudantes = ["Maria", "José", "Fernanda", "Catarina", "Elizabeth"];
+
+//nomeEstudantes[nomeEstudantes.length] = "Patricia";
+nomeEstudantes.push("Laís"); // faz o mesmo que a linha 5
+nomeEstudantes.push("Mariza");
+
+console.log(nomeEstudantes);
+
+nomeEstudantes.pop(); // tira o último do array
+
+console.log(nomeEstudantes); 
