02aa45ded87f8779acacdf339e6e1a4454b3c83f
cybergrazi <graziellecvergilio@gmail.com>
2023-07-23 19:19:32 -0300

    Introdução à estrutura de repetição for of

---
+++ unidade-1/aula-05-arrays-e-loops/for-of.js
@@ -0,0 +1,7 @@
+// Introdução à estrutura de repetição, o tipo de for: For Of.
+
+const nomeEstudantes = ["Maria", "José", "Fernanda", "Catarina", "Elizabeth"];
+
+for (let nome of nomeEstudantes) { // cria uma variável "nome" para usar como indice
+    console.log(nome); // imprime cada índice do array
+}
