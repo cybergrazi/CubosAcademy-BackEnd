c431a43a755d5e51a2e6e39f58d926806ceda2a1
cybergrazi <graziellecvergilio@gmail.com>
2023-07-23 18:32:33 -0300

    Introdução à estrutura de repetição for tradicion

---
+++ unidade-1/aula-05-arrays-e-loops/for-de-c.js
@@ -0,0 +1,15 @@
+// Introdução à estrutura de repetição, um tipo de for.
+
+const nomeEstudantes = ["Maria", "José", "Fernanda", "Catarina", "Elizabeth"];
+
+for (let i = 0; i < 5; i++) {
+    console.log(nomeEstudantes[i]);
+}
+
+// funciona como o código da aula de while:
+// let indice = 0;
+
+// while (indice < nomeEstudantes.length) {
+//     console.log(nomeEstudantes[indice]);
+//     indice++;
+// }
