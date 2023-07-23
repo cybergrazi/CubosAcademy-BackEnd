1ec15ddf71a7542441b28b5de24b3bfca27f3f2e
cybergrazi <graziellecvergilio@gmail.com>
2023-07-23 18:20:32 -0300

    Introdução à estrutura de repetição while

---
+++ unidade-1/aula-05-arrays-e-loops/while.js
@@ -0,0 +1,10 @@
+// Introdução à estrutura de repetição while.
+
+const nomeEstudantes = ["Maria", "José", "Fernanda", "Catarina", "Elizabeth"];
+
+let indice = 0;
+
+while (indice < nomeEstudantes.length) {
+    console.log(nomeEstudantes[indice]);
+    indice++;
+}
