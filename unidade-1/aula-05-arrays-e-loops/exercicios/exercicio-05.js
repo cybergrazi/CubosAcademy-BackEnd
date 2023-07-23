7daf90bdea04a90e4b88aacfc389598242942e55
cybergrazi <graziellecvergilio@gmail.com>
2023-07-23 19:54:51 -0300

    exercício 5 resolvido

---
+++ unidade-1/aula-05-arrays-e-loops/exercicios/exercicio-05.js
@@ -0,0 +1,14 @@
+// Faça um programa que conte de 10 até 0 (de trás pra frente mesmo).
+
+let numero = 10;
+
+while (numero >= 0) {
+    console.log(numero);
+    numero--;
+}
+
+// O professor resolveu:
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+// }
