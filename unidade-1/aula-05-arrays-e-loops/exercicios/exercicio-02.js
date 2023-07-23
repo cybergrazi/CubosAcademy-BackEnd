070d4770937d46385bcc833261a0e2708663037e
cybergrazi <graziellecvergilio@gmail.com>
2023-07-23 18:38:01 -0300

    exercício 2 resolvido

---
+++ unidade-1/aula-05-arrays-e-loops/exercicios/exercicio-02.js
@@ -0,0 +1,15 @@
+// Faça um programa que imprima todos os elementos de um array que contém alguns 
+// numeros quaisquer.
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+let i = 0;
+
+// for (i = 0; i < numeros.length; i++) {
+//     console.log(numeros[i]);
+// }
+
+while (i < numeros.length) {
+    console.log(numeros[i]);
+    i++
+}
