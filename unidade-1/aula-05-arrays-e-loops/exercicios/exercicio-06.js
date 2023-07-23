336c28f19485c7f3a92166363a940442b41c1fe1
cybergrazi <graziellecvergilio@gmail.com>
2023-07-23 20:02:00 -0300

    exercício 6 resolvido

---
+++ unidade-1/aula-05-arrays-e-loops/exercicios/exercicio-06.js
@@ -0,0 +1,8 @@
+// Faça um programa que imprima todos os números pares entre 1 e 50.
+
+let numeros = 2;
+
+while (numeros <= 50 && numeros % 2 === 0) {
+    console.log(numeros);
+    numeros += 2;
+}
