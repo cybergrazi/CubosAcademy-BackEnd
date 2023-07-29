a4f6d3177d3d7a95a93249528841202ff1ab0b3e
cybergrazi <graziellecvergilio@gmail.com>
2023-07-29 11:56:03 -0300

    Introdução ao conceito de objetos

---
+++ unidade-1/aula-06-objetos/intro.js
@@ -0,0 +1,10 @@
+// Introdução ao conceito de objetos.
+
+const pessoa = {  // é assim que fica guardado o objeto.
+    nome: "Grazi",
+    altura: 1.69,
+    peso: 75
+};
+
+console.log(pessoa);
+console.log(`Meu nome é ${pessoa["nome"]}, minha altura é ${pessoa.altura} e meu peso é ${pessoa.peso}.`)
