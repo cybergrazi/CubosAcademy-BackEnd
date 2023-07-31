7481ba8bce8f7078a15b3aac32488055028ed20d
cybergrazi <graziellecvergilio@gmail.com>
2023-07-31 18:04:25 -0300

    Introdução às funções

---
+++ unidade-1/aula-07-funcoes/intro.js
@@ -0,0 +1,10 @@
+// Introdução às funções.
+// Criar uma função que imprima olá na tela.
+
+
+imprimirOla() // este é o comando para executar.
+
+function imprimirOla() { // aqui apenas declara a função
+    console.log("Olá");
+}
+// ele pode ser declarado depois!!
