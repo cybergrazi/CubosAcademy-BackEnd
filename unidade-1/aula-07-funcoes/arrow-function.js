8654e68de5de6d67f00293e7ab607503187b0806
cybergrazi <graziellecvergilio@gmail.com>
2023-08-01 20:10:51 -0300

    Como criar funções arrow function

---
+++ unidade-1/aula-07-funcoes/arrow-function.js
@@ -0,0 +1,8 @@
+// Como criar funções arrow function.
+
+const soma = (x, y) => { //isso é igual a "function nome () {}"
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(5, 3));
