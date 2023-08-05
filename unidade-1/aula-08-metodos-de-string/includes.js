e70cf806ab8092b9d554337f87026594d2b9d51f
cybergrazi <graziellecvergilio@gmail.com>
2023-08-05 17:34:41 -0300

    Introdução ao método includes

---
+++ unidade-1/aula-08-metodos-de-string/includes.js
@@ -0,0 +1,5 @@
+// Introdução ao método includes.
+
+const frase = "Eu estou aprendendo a programar na Cubos Academy.";
+
+console.log(frase.includes("Cubos", 10)); // após a vírgula, é a posição inicial de busca. Sempre precisa ser um number.
