fb9691dcbcfe89958529b28655a358eadaf2459a
cybergrazi <graziellecvergilio@gmail.com>
2023-08-08 14:15:44 -0300

    Introdução ao método reverse

---
+++ unidade-1/aula-09-metodos-de-string-i/reverse.js
@@ -0,0 +1,9 @@
+// Introdução ao método reverse.
+
+const numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+
+const resultado = numeros.reverse();
+
+console.log(resultado);
+console.log(numeros); // muta também o original.
