0aac4041e19ffdd9d12e9bf5ae9c1ada9ffb33cc
cybergrazi <graziellecvergilio@gmail.com>
2023-08-13 20:37:28 -0300

    Introdução ao método map

---
+++ unidade-1/aula-10-metodos-de-string-ii/map.js
@@ -0,0 +1,16 @@
+// Introdução ao método map.
+
+const usuarios = [
+    { nome: "Guido", sobrenome: "Cerqueira", idade: 31 },
+    { nome: "Daniel", sobrenome: "Lopes", idade: 29 },
+    { nome: "Vitor", sobrenome: "Vidal", idade: 28 }
+];
+
+const novoArray = usuarios.map((usuario) => {
+    return {
+        nomeCompleto: `${usuario.nome} ${usuario.sobrenome}`,
+        idade: usuario.idade
+    }
+});
+
+console.log(novoArray);
