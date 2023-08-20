16dd7b2423cc26552cfcdb2621bfb54cc0cca994
cybergrazi <graziellecvergilio@gmail.com>
2023-08-20 18:47:56 -0300

    exercício 01 resolvido

---
+++ unidade-1/aula-11-metodos-de-arrays-iii/exercicios/exercicio-01.js
@@ -0,0 +1,22 @@
+// Faça a ordenação dos resultados de forma crescente, segundo o id de cada usuário,
+// depois faça a ordenação decrescente.
+
+const usuarios = [
+    { id: 11, nome: "joao", idade: 23 },
+    { id: 2, nome: "maria", idade: 18 },
+    { id: 4, nome: "ana", idade: 30 },
+    { id: 1, nome: "rodrigo", idade: 17 },
+    { id: 123, nome: "rodrigo", idade: 17 }
+];
+
+usuarios.sort((a, b) => {
+    return a.id - b.id;
+});
+
+console.log(usuarios);
+
+usuarios.sort((a, b) => {
+    return b.id - a.id;
+});
+
+console.log(usuarios);
